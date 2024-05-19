a337670def59516f69227291746a10b51d1f121a
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 16:36:27 -0700

    add remaining cars,
    still need to correctly implement, with correct parts

--- carFactory.py
+++ carFactory.py
@@ -11,4 +11,19 @@ class carFactory:
         self.engine = CapuletEngine(last_service_mileage, current_mileage)
         self.battery = SpindlerBattery(last_service_date, current_date)
         self.car = Car(self.engine, self.battery)
-    
+    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        self.engine = CapuletEngine(last_service_mileage, current_mileage)
+        self.battery = SpindlerBattery(last_service_date, current_date)
+        self.car = Car(self.engine, self.battery)
+    def create_palindrome(self, current_date, last_service_date, warning_light_on):
+        self.engine = CapuletEngine(last_service_mileage, current_mileage)
+        self.battery = SpindlerBattery(last_service_date, current_date)
+        self.car = Car(self.engine, self.battery)
+    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        self.engine = CapuletEngine(last_service_mileage, current_mileage)
+        self.battery = SpindlerBattery(last_service_date, current_date)
+        self.car = Car(self.engine, self.battery)
+    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        self.engine = CapuletEngine(last_service_mileage, current_mileage)
+        self.battery = SpindlerBattery(last_service_date, current_date)
+        self.car = Car(self.engine, self.battery)
