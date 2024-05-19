6fafa431e977b4335d650f7bf336da8d4d11afcd
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 16:22:57 -0700

    implement battery class
    also add spindler battery

---
+++ battery.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class Battery(ABC):
+    def __init__(self):
+        pass
+    @abstractmethod
+    def needs_service(self):
+        return True
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, lastServiceDate, currentDate):
+        Battery.__init__(self)
+        self.lastServiceDate = lastServiceDate
+        self.currentDate = currentDate
+
+    def needs_service(self):
+        return (self.currentDate.year - self.lastServiceDate.year)>=2
+
