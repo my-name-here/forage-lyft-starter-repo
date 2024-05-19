fd444cadf5448e03e12b8d81877e34fe5eeeb331
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 16:11:18 -0700

    change where implementing
    instead of implementing each engine in it's own file, do it all in engine file

--- engine.py
+++ engine.py
@@ -7,3 +7,13 @@ class Engine(ABC):
     @abstractmethod
     def needs_service(self):
         return True
+
+
+class CapuletEngine(Engine):
+    def __init__(self, lastServiceMilage, currentMilage):
+        Engine.__init__(self)
+        self.lastServiceMilage = lastServiceMilage
+        self.currentMilage = currentMilage
+
+    def needs_service(self):
+        return (self.currentMilage - self.lastServiceMilage)>30000
