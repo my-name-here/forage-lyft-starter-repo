9698823a517e621011b977a38b2dc6465099b3c6
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 16:43:49 -0700

    add servicable class
    adjust car to be a subclass

--- car.py
+++ car.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from Servicable import Servicable
 
-
-class Car(ABC):
+class Car(Servicable, ABC):
     def __init__(self, engine, battery):
+        super().__init__()
         self.battery = battery
         self.engine = engine
 
