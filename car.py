74d19399340d29de2909b141a80c1f63d0dfde87
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 18:18:38 -0700

    pass wearArray to each function, add tiretype var
    will use this to create cars with tire data

--- car.py
+++ car.py
@@ -10,4 +10,4 @@ class Car(Servicable, ABC):
 
     @abstractmethod
     def needs_service(self):
-        return (self.engine.needs_service() or self.battery.needs_service() )
+        return (self.engine.needs_service() or self.battery.needs_service())
--- carFactory.py
+++ carFactory.py
@@ -3,27 +3,28 @@ from car import Car
 from engine import *
 from battery import *
 class CarFactory:
-    def __init__(self):
+    def __init__(self, tireType):
         self.engine = None
         self.battery = None
         self.car = None
-    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+        self.tireType = tireType
+    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage, wearArray):
         self.engine = CapuletEngine(last_service_mileage, current_mileage)
         self.battery = SpindlerBattery(last_service_date, current_date)
         self.car = Car(self.engine, self.battery)
-    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage, wearArray):
         self.engine = WilloughbyEngine(last_service_mileage, current_mileage)
         self.battery = SpindlerBattery(last_service_date, current_date)
         self.car = Car(self.engine, self.battery)
-    def create_palindrome(self, current_date, last_service_date, warning_light_on):
+    def create_palindrome(self, current_date, last_service_date, warning_light_on, wearArray):
         self.engine = SternmanEngine(warning_light_on)
         self.battery = SpindlerBattery(last_service_date, current_date)
         self.car = Car(self.engine, self.battery)
-    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage, wearArray):
         self.engine = WilloughbyEngine(last_service_mileage, current_mileage)
         self.battery = NubbinBattery(last_service_date, current_date)
         self.car = Car(self.engine, self.battery)
-    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage, wearArray):
         self.engine = CapuletEngine(last_service_mileage, current_mileage)
         self.battery = NubbinBattery(last_service_date, current_date)
         self.car = Car(self.engine, self.battery)
