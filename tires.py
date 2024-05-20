3d71ec6eea5ad82890cff45e4133598a6a780ea6
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 18:01:52 -0700

    create tires.py, add CarriganTires
    used for tire classes

---
+++ tires.py
@@ -0,0 +1,14 @@
+class Tires(ABC):
+    def __init__(self, wearArray):
+        self.wearArray = wearArray
+
+    @abstractmethod
+    def needs_service(self):
+        return True
+
+class CarriganTires(Tires):
+    def __init__(self, wearArray):
+        super.__init__(self, wearArray)
+    def needs_service(self):
+        TireNeedsService = [tireWear>=0.9 for tireWear in self.wearArray]
+        return True in TireNeedsService
