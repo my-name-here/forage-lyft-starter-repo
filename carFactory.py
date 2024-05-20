9147e902f84c5e576735d2f84b76749d4e05ea2e
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 18:19:30 -0700

    pass wearArray when creating cars
    will pass tire type next as well

--- carFactory.py
+++ carFactory.py
@@ -11,20 +11,20 @@ class CarFactory:
     def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage, wearArray):
         self.engine = CapuletEngine(last_service_mileage, current_mileage)
         self.battery = SpindlerBattery(last_service_date, current_date)
-        self.car = Car(self.engine, self.battery)
+        self.car = Car(self.engine, self.battery, wearArray)
     def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage, wearArray):
         self.engine = WilloughbyEngine(last_service_mileage, current_mileage)
         self.battery = SpindlerBattery(last_service_date, current_date)
-        self.car = Car(self.engine, self.battery)
+        self.car = Car(self.engine, self.battery, wearArray)
     def create_palindrome(self, current_date, last_service_date, warning_light_on, wearArray):
         self.engine = SternmanEngine(warning_light_on)
         self.battery = SpindlerBattery(last_service_date, current_date)
-        self.car = Car(self.engine, self.battery)
+        self.car = Car(self.engine, self.battery, wearArray)
     def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage, wearArray):
         self.engine = WilloughbyEngine(last_service_mileage, current_mileage)
         self.battery = NubbinBattery(last_service_date, current_date)
-        self.car = Car(self.engine, self.battery)
+        self.car = Car(self.engine, self.battery, wearArray)
     def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage, wearArray):
         self.engine = CapuletEngine(last_service_mileage, current_mileage)
         self.battery = NubbinBattery(last_service_date, current_date)
-        self.car = Car(self.engine, self.battery)
+        self.car = Car(self.engine, self.battery, wearArray)
