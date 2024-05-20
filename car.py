210af1c7237621820ab64521188fb162eb656a53
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 18:14:29 -0700

    begin updating car class
    will check if tires need servicing when done

--- car.py
+++ car.py
@@ -2,11 +2,12 @@ from abc import ABC, abstractmethod
 from Servicable import Servicable
 
 class Car(Servicable, ABC):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, wearArray):
         super().__init__()
         self.battery = battery
         self.engine = engine
+        self.wearArray = wearArray
 
     @abstractmethod
     def needs_service(self):
-        return (self.engine.needs_service() or self.battery.needs_service())
+        return (self.engine.needs_service() or self.battery.needs_service() )
