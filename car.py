2f318b6d1cda0fbbc0e111a702a8acecb1e02755
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 18:23:40 -0700

    update car to check if tires need service,
    takes in tire type, and wear array, creating tire variable, and then checks if it needs service when we call needs_service

--- car.py
+++ car.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
 from Servicable import Servicable
-
+import tires
 class Car(Servicable, ABC):
-    def __init__(self, engine, battery, wearArray):
+    def __init__(self, engine, battery, wearArray, tireType):
         super().__init__()
         self.battery = battery
         self.engine = engine
         self.wearArray = wearArray
+        self.tire = tires.CarriganTires(wearArray) if tireType == "Carrigan" else tires.OctoprimeTires(wearArray)
 
     @abstractmethod
     def needs_service(self):
-        return (self.engine.needs_service() or self.battery.needs_service())
+        return (self.engine.needs_service() or self.battery.needs_service() or self.tire.needs_service())
