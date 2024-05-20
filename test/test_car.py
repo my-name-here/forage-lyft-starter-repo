6ef798131794748adead89e2f453f15d2adcb884
CB4747 <123422381+CB4747@users.noreply.github.com>
2024-05-19 17:13:16 -0700

    add remaining engine tests
    next up testing batteries

--- test/test_car.py
+++ test/test_car.py
@@ -8,14 +8,37 @@ class TestEngines(unittest.TestCase):
 
     def test_CapuletEngine_needsService(self):
         lastServiceMilage = 0
-        currentMilage = 31000
+        currentMilage = 30001
         Newengine = engine.CapuletEngine(lastServiceMilage, currentMilage)
         self.assertTrue(Newengine.needs_service())
 
-    def test_CapuletEngineDoesNotNeedService(self):
+    def test_CapuletEngine_DoesNotNeedService(self):
         lastServiceMilage = 0
-        currentMilage = 29000
+        currentMilage = 30000
         Newengine = engine.CapuletEngine(lastServiceMilage, currentMilage)
+        self.assertTrue(Newengine.needs_service())
+
+    def test_WilloughbyEngine_needsService(self):
+        lastServiceMilage = 0
+        currentMilage = 60001
+        Newengine = engine.WilloughbyEngine(lastServiceMilage, currentMilage)
+        self.assertTrue(Newengine.needs_service())
+
+    def test_WilloughbyEngine_DoesNotNeedService(self):
+        lastServiceMilage = 0
+        currentMilage = 60000
+        Newengine = engine.WilloughbyEngine(lastServiceMilage, currentMilage)
+        self.assertTrue(Newengine.needs_service())
+
+    def test_SternmanEngine_needsService(self):
+        warningLightOn = True
+        Newengine = engine.SternmanEngine(lastServiceMilage, currentMilage)
+        self.assertTrue(Newengine.needs_service())
+
+    def test_SternmanEngine_DoesNotNeedService(self):
+        warningLightOn = False
+
+        Newengine = engine.SternmanEngine(warningLightOn)
         self.assertTrue(Newengine.needs_service())
 
 
